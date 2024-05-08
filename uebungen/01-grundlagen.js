12aa505f7aad38d83b4289b98ad47e4bd3784515
Melina <156646846+bm156661@users.noreply.github.com>
2024-05-08 10:43:35 +0000

    Schleife erstellt

--- uebungen/01-grundlagen.js
+++ uebungen/01-grundlagen.js
@@ -1,6 +1,7 @@
 console.log('grundlagen');
 console.log('==========');
 
+
 console.log('Aufgabe 1');
 console.log('Erstelle zwei Variablen namens a und b. Initialisiere mit den Werten 1 und 2.');
 
@@ -35,6 +36,7 @@ let c = a + b;
 
 console.log('c hat den Wert:' + c);
 
+
 console.log('Aufgabe 5');
 console.log('a und b sind die Seitenlängen der Katheten eines rechtwinkligen Dreiecks. Bestimme die Länge der Hypotenuse c.');
 
@@ -62,7 +64,44 @@ console.log('Kapital nach einem Jahr: ' + kapitalNachEinemJahr + 'EUR.');
 let kapitalNachZweiJahren = kapitalNachEinemJahr * (1 + zinssatz);
 console.log('Kapital nach zwei Jahren: ' + kapitalNachZweiJahren + 'EUR.');
 
-let endkapital = Math.pow ((startkapital * zinssatz),laufzeit);
+let endkapital = startkapital * Math.pow ((1 + zinssatz),laufzeit);
 
 console.log('Endkapital nach ' + laufzeit + 'Jahren: ' + endkapital + 'EUR.');
 
+
+console.log('Aufgabe 7');
+console.log('Die Werte aus der vorherigen Aufgabe werden als Reihe dargstellt.');
+
+//Im Zeitpunkt Null ist das Endkapital gleich dem Startkapital.
+endkapital = startkapital; 
+console.log(endkapital);
+
+//Nach dem ersten Jahr erhöht sich das Endkapital um den Faktor 1,1
+endkapital = endkapital * (1 + zinssatz);
+console.log(endkapital);
+
+//Nach dem zweiten Jahr wird dem Endkapital der Wert des Endkapitals * 1,1 zugewiesen.
+endkapital = endkapital * (1 + zinssatz);
+console.log(endkapital);
+
+//Nach dem dritten Jahr wird dem Endkapital der Wert des Endkapitals * 1,1 zugewiesen.
+endkapital = endkapital * (1 + zinssatz);
+console.log(endkapital);
+
+//Nach dem vierten Jahr wird dem Endkapital der Wert des Endkapitals * 1,1 zugewiesen.
+endkapital = endkapital * (1 + zinssatz);
+console.log(endkapital);
+
+console.log('Aufgabe 8');
+console.log('In Aufgabe 7 wurde die Anweisung endkapital = ... mehrfach wiederholt. Um sich Tipparbeit zu sparen und die Wiederholung der Anweisung in der gewünschten Häufigkeit durchzuführen, nutzt der Programmierer eine Schleife.');
+
+
+startkapital = 100;
+endkapital = startkapital;
+zinssatz = 0.1;
+laufzeit = 3;
+
+for(let i = 0; i < laufzeit; i++) {
+    endkapital = endkapital * (1 + zinssatz);
+    console.log('Endkapital nach Jahr' + (i+1) + ': ' + endkapital + 'EUR.');   
+}
